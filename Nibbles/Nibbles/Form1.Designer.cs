63b44de58788ac28bbadb3458c129ccc3262b953
Ali Bizadi [alibizi] <60652838+alibizadi@users.noreply.github.com>
2021-01-03 11:59:35 +0330

    Add files via upload

---
+++ Nibbles/Nibbles/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Nibbles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.screen = new System.Windows.Forms.PictureBox();
+            this.resaultLBL = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.scoreLBL = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.screen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // screen
+            // 
+            this.screen.BackColor = System.Drawing.SystemColors.Highlight;
+            this.screen.Location = new System.Drawing.Point(12, 12);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(0, 0);
+            this.screen.TabIndex = 0;
+            this.screen.TabStop = false;
+            this.screen.Paint += new System.Windows.Forms.PaintEventHandler(this.Screen_Paint);
+            // 
+            // resaultLBL
+            // 
+            this.resaultLBL.AutoSize = true;
+            this.resaultLBL.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.resaultLBL.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.resaultLBL.Location = new System.Drawing.Point(100, 150);
+            this.resaultLBL.Name = "resaultLBL";
+            this.resaultLBL.Size = new System.Drawing.Size(0, 38);
+            this.resaultLBL.TabIndex = 1;
+            // 
+            // scoreLBL
+            // 
+            this.scoreLBL.AutoSize = true;
+            this.scoreLBL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.scoreLBL.Location = new System.Drawing.Point(507, 47);
+            this.scoreLBL.Name = "scoreLBL";
+            this.scoreLBL.Size = new System.Drawing.Size(0, 25);
+            this.scoreLBL.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(484, 307);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ESC For End!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 423);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.scoreLBL);
+            this.Controls.Add(this.resaultLBL);
+            this.Controls.Add(this.screen);
+            this.MaximumSize = new System.Drawing.Size(700, 470);
+            this.MinimumSize = new System.Drawing.Size(700, 470);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.screen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox screen;
+        private System.Windows.Forms.Label resaultLBL;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label scoreLBL;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Nibbles/Nibbles/Form1.cs
@@ -0,0 +1,234 @@
+﻿
+/*Nibbles Game Made By Ali Bizadi [alibizi]
+ Commect By The Author: C# is good but I more Love C++*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nibbles
+{
+    public partial class Form1 : Form
+    {
+        List<Square> Snakes = new List<Square>();
+        Square fruit;
+
+        public int keyChar = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            new Settings();
+
+            timer1.Interval = Settings.Speed;
+            timer1.Tick += new EventHandler(UpdateScreen);
+            timer1.Start();
+
+            screen.Width = 20 * Settings.Width;
+            screen.Height = 20 * Settings.Height;
+
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            resaultLBL.Visible = false;
+
+            new Settings();
+            timer1.Interval = Settings.Speed;
+
+            Snakes.Clear();
+
+            Square head = new Square();
+            head.X = screen.Width / 2;
+            head.Y = screen.Height / 2;
+            Snakes.Add(head);
+
+            GenerateFruit();
+        }
+
+        private void GenerateFruit()
+        {
+            int maxX = screen.Width;
+            int maxY = screen.Height;
+
+            Random rand = new Random();
+            fruit = new Square();
+            bool flag1 = false, flag2 = false;
+            while (true)
+            {
+                int x = rand.Next(0, maxX);
+                int y = rand.Next(0, maxY);
+                if (x % Settings.Width == 0)
+                {
+                    fruit.X = x;
+                    flag1 = true;
+                }
+                if (y % Settings.Height == 0)
+                {
+                    fruit.Y = y;
+                    flag2 = true;
+                }
+
+                for(int i = 0; i < Snakes.Count; i++)
+                {
+                    if (x == Snakes[i].X && y == Snakes[i].Y)
+                    {
+                        flag1 = false;
+                        break;
+                    }
+                }
+
+                if (flag1 && flag2)
+                    break;
+            }
+        }
+
+        private void UpdateScreen(object sender, EventArgs e)
+        {
+            if (Settings.GameOver)
+            {
+                if (keyChar == 5)
+                {
+                    StartGame();
+                    keyChar = 0;
+                }
+            }
+            else
+            {
+                if (keyChar == 3/*100*/ && Settings.Dir != Direction.Left)         //RightArrow
+                    Settings.Dir = Direction.Right;
+                else if (keyChar == 1/*97*/ && Settings.Dir != Direction.Right)    //LeftArrow
+                    Settings.Dir = Direction.Left;
+                else if (keyChar == 2/*119*/ && Settings.Dir != Direction.Down)    //UpArrow
+                    Settings.Dir = Direction.Up;
+                else if (keyChar == 4/*115*/ && Settings.Dir != Direction.Up)      //DownArrow
+                    Settings.Dir = Direction.Down;
+                else if (keyChar == 6)
+                    Settings.GameOver = true;
+
+                MoveSnake();
+            }
+
+            screen.Invalidate();
+        }
+
+        private void Screen_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics canvas = e.Graphics;
+
+            if (!Settings.GameOver)
+            {
+                for(int i = 0; i < Snakes.Count; i++)
+                {
+                    canvas.FillRectangle(Brushes.DarkGray, new Rectangle(Snakes[i].X,
+                        Snakes[i].Y, Settings.Width, Settings.Height));
+
+                    canvas.FillEllipse(Brushes.Red, new Rectangle(fruit.X,
+                        fruit.Y, Settings.Width, Settings.Height));
+                }
+            }
+            else
+            {
+                string resaultMSG = "GameOver :)\nYou Earned " + Settings.Score + " SCORE\nPress ENTER For New PLaying.";
+
+                resaultLBL.Text = resaultMSG;
+                resaultLBL.Visible = true;
+            }
+        }
+
+        private void MoveSnake()
+        {
+            for (int i = Snakes.Count - 1; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    switch (Settings.Dir)
+                    {
+                        case Direction.Down:
+                            Snakes[i].Y += Settings.Height;
+                            break;
+                        case Direction.Up:
+                            Snakes[i].Y -= Settings.Height;
+                            break;
+                        case Direction.Right:
+                            Snakes[i].X += Settings.Width;
+                            break;
+                        case Direction.Left:
+                            Snakes[i].X -= Settings.Width;
+                            break;
+                    }
+
+                    int maxX = screen.Width;
+                    int maxY = screen.Height;
+
+                    if(Snakes[i].X < 0 || Snakes[i].Y < 0
+                        || Snakes[i].X >= maxX || Snakes[i].Y >= maxY)
+                    {
+                        Settings.GameOver = true;
+                    }
+
+                    for(int j = 1; j < Snakes.Count; j++)
+                    {
+                        if (Snakes[i].X == Snakes[j].X && Snakes[i].Y == Snakes[j].Y)
+                        {
+                            Settings.GameOver = true;
+                        }
+                    }
+
+                    if (Snakes[0].X == fruit.X && Snakes[0].Y == fruit.Y)
+                    {
+                        EatFruit();
+                    }
+                }
+                else
+                {
+                    Snakes[i].X = Snakes[i - 1].X;
+                    Snakes[i].Y = Snakes[i - 1].Y;
+                }
+            }
+        }
+
+        private void EatFruit()
+        {
+            Square tail = new Square();
+            tail.X = Snakes[Snakes.Count - 1].X;
+            tail.Y = Snakes[Snakes.Count - 1].Y;
+            Snakes.Add(tail);
+
+            Settings.Score += Settings.Points;
+            scoreLBL.Text = "SCORE = " + Settings.Score.ToString();
+
+            if (Settings.Speed > 70)
+            {
+                Settings.Speed -= 5;
+                timer1.Interval = Settings.Speed;
+            }
+
+            GenerateFruit();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+                keyChar = 3;
+            else if (e.KeyCode == Keys.Left)
+                keyChar = 1;
+            else if (e.KeyCode == Keys.Up)
+                keyChar = 2;
+            else if (e.KeyCode == Keys.Down)
+                keyChar = 4;
+            else if (e.KeyCode == Keys.Enter)
+                keyChar = 5;
+            else if (e.KeyCode == Keys.Escape)
+                keyChar = 6;
+        }
+    }
+}
